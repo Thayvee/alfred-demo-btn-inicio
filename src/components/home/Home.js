7cde39c0fc5059f972df36545cf0a71281be08dc
Thay M <contactothayra@gmail.com>
2023-05-24 11:46:59 -0500

    first commit

---
+++ src/components/home/Home.js
@@ -0,0 +1,18 @@
+import "./Home.css";
+
+// import banner from '../../images/banner.png';
+
+function Home() {
+  return (
+    <div id="home">
+      <h1>
+        Hola, <br />
+        soy Alfred
+      </h1>
+      <h2 className="h4">¿En qué puedo ayudarte?</h2>
+      <h2 className="h3">Visítanos en nuestro stand de ProXymo</h2>
+    </div>
+  );
+}
+
+export default Home;
